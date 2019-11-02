11eab5c4b3733e88b83ff501eeed1a1bdce658a4
Logan Connolly <logan.connolly@protonmail.com>
2019-11-02 14:52:52 +0100

    Remove image delete option

--- posts/run.sh
+++ posts/run.sh
@@ -11,14 +11,3 @@ docker run -it --rm -v${PWD}:/render render:rmd
 
 # Copy output report to readme so that it renders in github
 cp ${1}.md readme.md
-
-# Remove image
-while getopts "d" OPTION
-do
-        case $OPTION in
-                d)
-                        docker rmi -f $2
-                        exit
-                        ;;
-        esac
-done
